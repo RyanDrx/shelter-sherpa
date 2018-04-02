ea0af82b6dfaec47e671294647511560cc19c8e0
RyanDrx <Ryan.Dierickx@gmail.com>
2018-04-02 16:46:49 -0700

    Update naming

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ShelterSherpa
+# Material Design Template
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.3.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('shelter-sherpa App', () => {
+describe('material-design-template App', () => {
   let page: AppPage;
 
   beforeEach(() => {
