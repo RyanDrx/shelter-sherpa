d05e766490698f4a9ac410ecc6e8c54889f7d3d3
RyanDrx <Ryan.Dierickx@gmail.com>
2018-04-02 17:53:42 -0700

    Add basic animation capability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,44 @@
 import { MediaMatcher } from '@angular/cdk/layout';
 import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import {
+  trigger,
+  transition,
+  useAnimation,
+  state,
+  style
+} from '@angular/animations';
+import { rotateIn, zoomIn } from 'ng-animate';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('rotate', [
+      state('active', style({})),
+      transition(
+        '* => active',
+        useAnimation(rotateIn, {
+          params: { timing: 0.3 }
+        })
+      ),
+      state('inactive', style({})),
+      transition(
+        '* => inactive',
+        useAnimation(zoomIn, {
+          params: { timing: 0.3 }
+        })
+      )
+    ])
+  ]
 })
 export class AppComponent implements OnDestroy {
+  rotate: any;
+  state = 'inactive';
   title = 'app';
-
   mobileQuery: MediaQueryList;
 
-  fillerNav = Array(50)
+  fillerNav = Array(10)
     .fill(0)
     .map((_, i) => `Nav Item ${i + 1}`);
 
@@ -31,6 +59,10 @@ export class AppComponent implements OnDestroy {
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  toggleState() {
+    this.state = this.state === 'active' ? 'inactive' : 'active';
   }
 
   ngOnDestroy(): void {
